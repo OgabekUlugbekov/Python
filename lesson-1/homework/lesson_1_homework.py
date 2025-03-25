191f30504e00d9a9aea2df5288efd584d08aad42
OgabekUlugbekov <ogabekakmalovich@gmail.com>
2025-03-25 15:55:49 +0500

    Add files via upload

---
+++ lesson-1/homework/lesson_1_homework.py
@@ -0,0 +1,24 @@
+# 1)
+side = float(input("Enter the side of the square: "))
+P = 4 * side
+S = side ** 2
+print(f"P: {P}, S: {S}")
+
+# 2)
+diameter = float(input("Enter the diameter of the circle: "))
+pi = 3.1416
+length = pi * diameter
+print(f"length: {length}")
+
+# 3)
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+mean = (a + b) / 2
+print(f"Mean: {mean}")
+
+# 4)
+sum = a + b
+product = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(f"Sum: {sum}, Product: {product}, Square of a: {square_a}, Square of b: {square_b}")
